c3ac3a6208eb377a45aff1ef93cdf884756404bb
Katie Garner <kgarner3@students.towson.edu>
2026-04-18 18:00:34 -0400

    Update database.py
    Adding column for mustChangePassword to correct first-time login functionality.

--- database.py
+++ database.py
@@ -17,28 +17,30 @@ def init_db():
     #This is where the actual SQL is placed to do operations on the DB
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS customers (
-            Customer_ID   INTEGER PRIMARY KEY AUTOINCREMENT,
-            first_name    TEXT NOT NULL,
-            last_name     TEXT NOT NULL,
-            email         TEXT UNIQUE NOT NULL,
-            number        TEXT,
-            address       TEXT,
-            username      TEXT UNIQUE NOT NULL,
-            passwordHash  TEXT NOT NULL,
-            salt          TEXT NOT NULL
+            Customer_ID           INTEGER PRIMARY KEY AUTOINCREMENT,
+            first_name            TEXT NOT NULL,
+            last_name             TEXT NOT NULL,
+            email                 TEXT UNIQUE NOT NULL,
+            number                TEXT,
+            address               TEXT,
+            username              TEXT UNIQUE NOT NULL,
+            passwordHash          TEXT NOT NULL,
+            salt                  TEXT NOT NULL,
+            mustChangePassword    INTEGER NOT NULL DEFAULT 0
         )
     ''')
 
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS employees (
-            Employee_ID   INTEGER PRIMARY KEY AUTOINCREMENT,
-            first_name    TEXT NOT NULL,
-            last_name     TEXT NOT NULL,
-            email         TEXT UNIQUE NOT NULL,
-            username      TEXT UNIQUE NOT NULL,
-            passwordHash  TEXT NOT NULL,
-            salt          TEXT NOT NULL,
-            expenses      REAL NOT NULL DEFAULT 0.0
+            Employee_ID           INTEGER PRIMARY KEY AUTOINCREMENT,
+            first_name            TEXT NOT NULL,
+            last_name             TEXT NOT NULL,
+            email                 TEXT UNIQUE NOT NULL,
+            username              TEXT UNIQUE NOT NULL,
+            passwordHash          TEXT NOT NULL,
+            salt                  TEXT NOT NULL,
+            expenses              REAL NOT NULL DEFAULT 0.0
+            mustChangePassword    INTEGER NOT NULL DEFAULT 0
         )
     ''')
 
